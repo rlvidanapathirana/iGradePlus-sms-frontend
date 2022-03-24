f6066810eb7f0b359ee4e98e3660bb1cda9b450d
Asanka Gamage <asankagamage1998@gmail.com>
2022-03-24 11:42:17 +0530

    created event add/edit/ andreport page

---
+++ src/views/events/Event.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="card-body">
+                <h4> Event List </h4>
+
+                <router-link to="add-event">
+                    <button class="btn btn-primary mt-3"><i class="fa fa-plus mx-2" aria-hidden="true"></i> Add new Event</button>
+                </router-link>
+
+                <div class="mt-3">
+                    <form class="form-inline my-2 my-lg-0">
+                        <input v-model="search" class="form-control w-75 p-3 mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+
+
+                <div class="mt-3">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Capacity</th>
+                            <th scope="col">Width</th>
+                            <th scope="col">Length</th>
+                            <th scope="col">Area</th>
+                            <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">A1</th>
+                            <td>250</td>
+                            <td>5'</td>
+                            <td>12'</td>
+                            <td>16 Feet <sup>2</sup></td>
+                            <td>
+                                <button class="btn my-0 py-0"><i class="fas fa-edit"/></button>
+                                <button class="btn my-0 py-0"><i class="fa fa-trash"/></button>
+                            </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+</template>
