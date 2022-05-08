cfa32204a61100dcadbe501779fd2ddbfaeb12d5
Lakshan Vidanapathirana <rlvidanapathirana@gmail.com>
2022-05-08 18:47:00 +0530

    report pdf updated

--- src/views/teacher/Report.vue
+++ src/views/teacher/Report.vue
@@ -82,16 +82,16 @@ mounted() {
             } );
             //console.log(this.userArray);
             var img = new Image()
-            img.src = 'https://i.ibb.co/dJ2bJ3X/5512.jpg'
+            img.src = 'https://i.ibb.co/7VLScqs/staff-report.png'
             // Use of Date.now() method
               var d = Date(Date.now());
             // Converting the number of millisecond
             // in date string
             var a = d.toString()
-            doc.addImage(img, 'jpg', 75,150,80,80)
-            doc.text("Teacher Report", 10, 10);
+            doc.addImage(img, 'jpg', 5,160,200,80)
+            doc.text("Teacher Report", 15, 10);
             doc.setFontSize(10);
-            doc.text(a, 10, 140);
+            doc.text(a, 10, 150);
             // Or use javascript directly using table auto :
             autoTable(doc, {
             head: [['First Name', 'Last Name', 'Address Line','City','Mobile Number','Fix Number','Subject']],
